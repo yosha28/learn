5002c109c395350200f4e0268be29a1ab7813cd0
yosha28 <56972122+yosha28@users.noreply.github.com>
2019-10-29 16:56:56 +0200

    strchr
    в parseItem заменила strstr ,на strchr.Во всех скачанных проектах из learn strstr не работала.

--- 201ClassInCpp/ClassInCpp/strutils.cpp
+++ 201ClassInCpp/ClassInCpp/strutils.cpp
@@ -14,8 +14,9 @@ void nullStr(char *str, int count) {
 char *parseItem(char *fromChar, const char delimiter, char *parsedItem) {
 	nullStr(parsedItem, defaultNameLength);
 	char *result = NULL;
-	if (NULL != fromChar) {
-		result = strstr(fromChar, &delimiter);
+	if (NULL != fromChar)
+	{
+		result=strchr(fromChar,delimiter);//именила strstr ,у меня она не хотела работать никак
 		if (NULL != result) {
 			for (int i = 0; i < (result - fromChar); i++) {
 				*(parsedItem + i) = *(fromChar + i);
